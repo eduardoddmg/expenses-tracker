86636300972266015850641b803b1669bfbdef6c
Eduardo Melo <92065918+eduardoddmg@users.noreply.github.com>
2023-02-05 09:43:36 -0300

    update api config

--- frontend/src/utils/api.jsx
+++ frontend/src/utils/api.jsx
@@ -2,5 +2,5 @@ import axios from 'axios';
 
   // baseURL: "http://localhost:3001/api"
 export const api = axios.create({
-  baseURL: "https://expanses-tracker.herokuapp.com/api",
+  baseURL: "https://contatos-mern.onrender.com/api",
 });
